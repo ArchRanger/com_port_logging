4ea129153eb907c1f005e931934efd401fb76996
ArchRanger <86490747+ArchRanger@users.noreply.github.com>
2024-01-03 20:00:58 +0300

    Update com_port_scanning.py

--- com_port_scanning.py
+++ com_port_scanning.py
@@ -1,50 +1,46 @@
-﻿import serial.tools.list_ports  # serial kütüphanesi eklendi
-import time  # zaman işlemleri için time kütüphanesi eklendi
+﻿import serial.tools.list_ports 
+import time 
 import datetime
 
-# Ana Port Tarama Modülü
+
 class COMPorts:
-    def __init__(self, data: list):  # dışarıdan alacağı değerleri liste halinde "data" değişkeni olarak tutar
+    def __init__(self, data: list):  
         self.data = data
 
     @classmethod
-    def get_com_ports(cls):  # portbilgileri
+    def get_com_ports(cls):  
         data = []
-        ports = list(serial.tools.list_ports.comports())  # portları tarayıp liste halinde toplayarak "ports"
-          # değişkeninde tutar
+        ports = list(serial.tools.list_ports.comports())  
+          
 
-        # for loop ile ihtiyacımız olan cihaz bilgilerinin getirilmesi sağlandı, port
-        # ve cihaz değişimleri anlık yansımaktadır.
-
-        for port_ in ports:  # aşağıdaki obje sınıfına uygun şekilde içine "port, özellikleriyle beraber"
-                             # tanımlanarak oluşturulur
+        for port_ in ports:  
             obj = Object(data=dict({"device": port_.device, "serial_number": port_.serial_number,
                                     "description": port_.description.split("(")[0].strip()}))
-            data.append(obj)  # oluşturulan obje yukarıdaki listeye eklendi
-        return cls(data=data)  # class method özelliği olarak bu liste de __init__'e geri yollandı.
+            data.append(obj)  
+        return cls(data=data)  
 
 
-class Object:  # portu obje haline getirmek
-    def __init__(self, data: dict):  # ilgili objeler Public erişime açık hale getirildi
+class Object:  
+    def __init__(self, data: dict): 
         self.data = data
         self.device = data.get("device")
         self.description = data.get("description")
         self.serial_number = data.get("serial_number")
 
 
-class Time:  # Zaman döngüsü öenmli olduğu için ilgili yerlerde kullanım için hazır.
+class Time:  
     def time_now(self):
         t = time.localtime()
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)  # yıl-ay-gün-saat-dk-sn
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)  
 
         return current_time
 
 
 class Time_modifier:
 
-    def __init__(self,h,m,s):  # h,m ve s, class'ı çağırdığımız yerde başlattığımız saat bilgileri oluyor,
-        self.h = datetime.datetime.now().time().hour  # ve bunları içerisinde otomatik olarak işleyerek aşağıdaki
-        self.m = datetime.datetime.now().time().minute  # formatta yazdırıyor.
+    def __init__(self,h,m,s):  
+        self.h = datetime.datetime.now().time().hour  
+        self.m = datetime.datetime.now().time().minute  
         self.s = datetime.datetime.now().time().second
         if self.h > h:
             self.h = abs(self.h - h)
